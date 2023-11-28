dfc5e7c5e593ab7a15738ea6c58b288af5580def
S0627 <S0627@22S0627>
2023-11-28 14:17:59 +0900

    michome

---
+++ Microf/src/main/java/com/example/demo/MicHomeController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicHomeController {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@RequestMapping(path = "/michome", method = RequestMethod.GET)
+	public String Get(Model model) {
+
+		List<Map<String, Object>> todoList;
+
+		todoList = jdbcTemplate.queryForList("SELECT * FROM todo");
+
+		model.addAttribute("Result", todoList);
+
+		return "michome";
+	}
+
+	@RequestMapping(path = "/michome", method = RequestMethod.POST)
+	public String Post(Model model) {
+
+		return "michome";
+	}
+}
