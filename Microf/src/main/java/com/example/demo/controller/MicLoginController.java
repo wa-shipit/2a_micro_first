25135656aa997e44f6fde6de180fc45e6c2c80b6
hori-moon <horikozsky@gmail.com>
2023-12-04 09:47:37 +0900

    session追加

--- Microf/src/main/java/com/example/demo/controller/MicLoginController.java
+++ Microf/src/main/java/com/example/demo/controller/MicLoginController.java
@@ -10,35 +10,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class MicLoginController {
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	@RequestMapping(path = "/miclogin", method = RequestMethod.GET)
 	public String login() {
 		return "miclogin";
 	}
-	
+
 	@RequestMapping(path = "/miclogin", method = RequestMethod.POST)
-	public String login(String micloginid,String micpw,Model model) {
-	
-		if(micloginid.length() > 16 || micpw.length() > 16) {
+	public String login(String micloginid, String micpw, Model model, HttpSession session) {
+
+		if (micloginid.length() > 16 || micpw.length() > 16) {
 			String msg = "文字数が多すぎます";
 			model.addAttribute("moji", msg);
 			return "miclogin";
-		}else {
+		} else {
 			List<Map<String, Object>> resultList;
-			resultList = jdbcTemplate.queryForList("select * from miclogin where loginid = ? and password = ?",micloginid,micpw);
+			resultList = jdbcTemplate.queryForList("select * from miclogin where loginid = ? and password = ?",
+					micloginid, micpw);
 
-			if(resultList.isEmpty()) {
+			if (resultList.isEmpty()) {
 				String msg = "ログインに失敗しました";
 				model.addAttribute("moji", msg);
 				return "miclogin";
-			}else {
+			} else {
+				session.setAttribute("loginparam1", micloginid);
+				session.setAttribute("loginparam2", micpw);
 				return "redirect:/michome";
 			}
-		}		
+		}
 	}
 }
