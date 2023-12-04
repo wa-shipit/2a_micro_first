2c093c36c380ad4a9c239a7976b39334eb725638
S0627 <S0627@22S0627>
2023-12-04 10:41:21 +0900

    56

--- Microf/src/main/java/com/example/demo/controller/MicLoginController.java
+++ Microf/src/main/java/com/example/demo/controller/MicLoginController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class MicLoginController {
 
@@ -23,7 +25,7 @@ public class MicLoginController {
 	}
 
 	@RequestMapping(path = "/miclogin", method = RequestMethod.POST)
-	public String processLogin(String micloginid, String micpw, Model model) {
+	public String processLogin(String micloginid, String micpw, Model model, HttpSession session) {
 
 		List<Map<String, Object>> userList;
 		userList = jdbcTemplate.queryForList("select * from miclogin where loginid = ? and password = ?", micloginid,
@@ -31,6 +33,10 @@ public class MicLoginController {
 
 		if (!userList.isEmpty() && userList.get(0).get("loginid").equals(micloginid)
 				&& userList.get(0).get("password").equals(micpw)) {
+
+			session.setAttribute("loginparam1", micloginid);
+			session.setAttribute("loginparam2", micpw);
+
 			return "redirect:/michome";
 		} else {
 			model.addAttribute("message", "ログインに失敗しました");
